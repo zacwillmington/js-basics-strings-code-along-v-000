c58e0eaa79c68c44e54db1ac5c4797a1b371afa4
Zac Willmington <zac.willmington@gmail.com>
2018-09-03 23:00:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
 
-let shortGreeting = "Welcome, " + currentUser;
+let shortGreeting = "Welcome, " + currentUser[0];
