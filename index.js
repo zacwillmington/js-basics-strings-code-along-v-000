e91f633f091eaa61a9bc65258896af328e835de2
Zac Willmington <zac.willmington@gmail.com>
2018-09-03 23:03:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Billy fing";
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+let shortGreeting = "Welcome, " + currentUser[0] + "!" ;
